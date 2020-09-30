ea086ae3aec5c05c524ebf3a5e054c7c9713377e
dafealos <dafealos@hotmail.com>
2020-09-29 21:53:55 -0500

    listo

--- clase_2.R
+++ clase_2.R
@@ -62,3 +62,42 @@ Clima <- extract(Bioclim, B_impatiens) %>% as.data.frame()
 # las unimos
 
 B_impatiens <- B_impatiens %>% bind_cols(Clima)
+
+## Generar el modelo
+
+Fit1 <- glm(Measurement ~ bio1 + I(bio1^2) + bio7 + I(bio7^2) + 
+                    bio12 + I(bio12^2) + bio15 + I(bio15^2), family = poisson, 
+            data = B_impatiens)
+
+#### Predictions
+
+Prediccion <- predict(Bioclim, Fit1, type = "response")
+plot(Prediccion, colNA = "black")
+
+## Mapas con raster y sf
+
+USA <- getData(name = "GADM", country = "USA", level = 1) %>% st_as_sf() %>% st_crop(B_impatiens)
+
+Prediccion_DF <- Prediccion %>% as("SpatialPixelsDataFrame") %>% 
+        as.data.frame()  %>% rename(Abundancia = layer)
+
+ggplot() + geom_tile(data = Prediccion_DF, aes(x = x, y = y,fill = Abundancia)) + #geom_tile para raster
+        geom_sf(data = Mapa, alpha = 0, color = "white") + theme_bw() + scale_fill_viridis_c() + xlab("") + ylab("")
+
+ggplot() + 
+        geom_tile(data = Prediccion_DF, aes(x = x, y = y,fill = Abundancia)) + 
+        geom_sf(data = USA, alpha = 0, color = "white") + 
+        geom_sf(data = B_impatiens, aes(size = Measurement)) + 
+        theme_bw() + scale_fill_viridis_c(option = "plasma") + 
+        theme(legend.position = "bottom") + xlab("") + ylab("")
+
+
+### Para predecir al futuro
+
+Futuro <- getData("CMIP5", var = "bio", res = 2.5, rcp = 85,model = "HD", year = 70) %>% 
+        crop(B_impatiens)
+Futuro <- Futuro[[c(1, 7, 12, 15)]]
+
+names(Futuro) <- c("bio1", "bio7", "bio12", "bio15")
+Futuro_pred <- predict(Futuro, Fit1, type = "response")
+plot(Futuro_pred, colNA = "black")
